b24d20a9329af6dc62960fb328afc270091a45a3
Dylan <52908667+smellycloud@users.noreply.github.com>
2025-01-02 15:33:34 +0100

    update main.py ensemble template

--- views_pipeline_core/templates/ensemble/template_main.py
+++ views_pipeline_core/templates/ensemble/template_main.py
@@ -20,28 +20,35 @@ def generate(script_dir: Path) -> bool:
         bool:
             True if the script was written and compiled successfully, False otherwise.
     """
+
     code = """import wandb
-import warnings
-from pathlib import Path
-from views_pipeline_core.cli.utils import parse_args, validate_arguments
-from views_pipeline_core.logging.utils import setup_logging
-from views_pipeline_core.managers.ensemble import EnsemblePathManager, EnsembleManager
+    import warnings
+    from pathlib import Path
+    from views_pipeline_core.cli.utils import parse_args, validate_arguments
+    from views_pipeline_core.managers.log import LoggingManager
+    from views_pipeline_core.managers.ensemble import EnsemblePathManager, EnsembleManager
 
-warnings.filterwarnings("ignore")
+    warnings.filterwarnings("ignore")
 
-try:
-    ensemble_path = EnsemblePathManager(Path(__file__))
-except Exception as e:
-    raise RuntimeError(f"An unexpected error occurred: {e}.")
+    try:
+        ensemble_path = EnsemblePathManager(Path(__file__))
+        logger = LoggingManager(ensemble_path).get_logger()
+    except FileNotFoundError as fnf_error:
+        raise RuntimeError(
+            f"File not found: {fnf_error}. Check the file path and try again."
+        )
+    except PermissionError as perm_error:
+        raise RuntimeError(
+            f"Permission denied: {perm_error}. Check your permissions and try again."
+        )
+    except Exception as e:
+        raise RuntimeError(f"Unexpected error: {e}. Check the logs for details.")
 
-logger = setup_logging(logging_path=ensemble_path.logging)
-
-
-if __name__ == "__main__":
+    if __name__ == "__main__":
     wandb.login()
     args = parse_args()
     validate_arguments(args)
 
     EnsembleManager(ensemble_path=ensemble_path).execute_single_run(args)
-"""
+    """
     return save_script(script_dir, code)
