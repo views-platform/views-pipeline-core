9e9dc75f3d89b979e5cc20b1ff2a76cff7f1007a
Dylan <52908667+smellycloud@users.noreply.github.com>
2025-02-01 20:37:39 +0100

    iterable fix attempt

--- views_pipeline_core/hpc/pandas.py
+++ views_pipeline_core/hpc/pandas.py
@@ -14,6 +14,20 @@ def import_pandas():
 
 pd = import_pandas()
 
+# Define a unified interface for common pandas/cudf operations
+def DataFrame(*args, **kwargs):
+    return pd.DataFrame(*args, **kwargs)
+
+def Series(*args, **kwargs):
+    return pd.Series(*args, **kwargs)
+
+def MultiIndex_from_product(*args, **kwargs):
+    if hasattr(pd, 'MultiIndex'):
+        return pd.MultiIndex.from_product(*args, **kwargs)
+    else:
+        import pandas as pds
+        return pds.MultiIndex.from_product(*args, **kwargs)
+
 # Dynamically set the attributes of the current module to match those of the imported library
 current_module = sys.modules[__name__]
 for attr in dir(pd):
