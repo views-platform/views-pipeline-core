355d92cf70a2f1c6313cfbad9480099b7ab847bc
Dylan <52908667+smellycloud@users.noreply.github.com>
2025-11-06 15:16:27 +0100

    targets fix

--- views_pipeline_core/managers/configuration.py
+++ views_pipeline_core/managers/configuration.py
@@ -264,6 +264,9 @@ class ConfigurationManager:
         if self.config_deployment:
             config.update(self.config_deployment)
         if self.config_meta:
+            if "targets" in self.config_meta:
+                if isinstance(self.config_meta["targets"], str):
+                    self.config_meta["targets"] = [self.config_meta["targets"]]
             config.update(self.config_meta)
         if self._runtime_config:
             config.update(self._runtime_config)
